1d074df8e87d79fa7ee44b214417aebb51133582
EmilSkov <EmilSkov@users.noreply.github.com>
2018-12-15 13:49:46 +0100

    finale update

---
+++ docs/flow4/oldsweetbot/stylesheets/javastyle.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+function toggle_visibility() {
+    var x = document.getElementById("sidebanner");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+		}
+
+function myFunction() {
+	var menunav = document.getElementById("mobilbaggrund");
+	if (menunav.style.display === "none") {
+		menunav.style.display = "block";
+	}
+	else {
+		menunav.style.display = "none";
+	}
+}
